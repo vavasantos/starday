89e114a8b2e3a47260c2ffe60c0b3ac4173d9cdf
Vanessa <santosvanessa1617@gmail.com>
2023-05-04 13:53:30 -0300

    Configuração inicial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ var porta = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 const path = require("path");
-const flash = require("flash");
+const flash = require("req-flash");
+var session = require("express-session");
 
+app.use(session({ secret: "123", resave: true, saveUninitialized: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
+
+app.set("view engine", "ejs");
+app.set("views", ".src/views");
+
+app.use(express.static(path.join("src", "public")));
+app.listen(porta);
